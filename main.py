e4a34c73579041a1a08e1a10f77f2fde980a9d46
0xlupo <mr.ysaj@gmail.com>
2022-04-03 12:19:08 +0430

    find solution of vectorize

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+# import gensim.models
 import gensim.downloader as api
 wv = api.load('word2vec-google-news-300')
 for index, word in enumerate(wv.index_to_key):
     if index == 10:
         break
     print(f"word #{index}/{len(wv.index_to_key)} is {word}")
+# sentences = "iran ey saraye omid"
+# model = gensim.models.Word2Vec(sentences=sentences)
+
+vec_king = wv['king']
+vec_woman = wv['woman']
+
+vec_queen = vec_king + vec_woman
+print(vec_queen)
+print(wv.most_similar(positive=['woman', 'king'], negative=['man'], topn=1))
+a =2
