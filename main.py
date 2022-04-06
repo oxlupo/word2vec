048774d9091a1e020bb19f8978814c9938f0a130
0xlupo <mr.ysaj@gmail.com>
2022-04-06 13:29:18 +0430

    find solution of vectorize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # import gensim.models
+from gensim.models import Word2Vec
+import pandas as pd
 import gensim.downloader as api
 wv = api.load('word2vec-google-news-300')
 for index, word in enumerate(wv.index_to_key):
@@ -14,4 +16,4 @@ vec_woman = wv['woman']
 vec_queen = vec_king + vec_woman
 print(vec_queen)
 print(wv.most_similar(positive=['woman', 'king'], negative=['man'], topn=1))
-a =2
+
